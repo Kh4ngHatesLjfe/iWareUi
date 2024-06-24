c2b86952b0599592dd5551f8ade36d1af36b78b2
Kh4ngHatesLjfe <149120276+Kh4ngHatesLjfe@users.noreply.github.com>
2024-06-24 13:00:54 -0700

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,11 +1,11 @@
-
- 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Kh4ngHatesLjfe/lib/main/lua.txt'))()
-task.wait(1.2)
-game.StarterGui:SetCore("SendNotification", {
-Title = "Discord";
-Text = ".gg/WZYxSedMMY";
-Duration = 5;
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Discord", -- Required
+	Text = "gg/WZYxSedMMYn", -- Required
+	Icon = "rbxassetid://18183970886" -- Optional
+  Duration = 5;
+  setclipboard("https://discord.gg/WZYxSedMMY")
 })
-setclipboard("https://discord.gg/WZYxSedMMY")
+ task.wait(1.2)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Kh4ngHatesLjfe/lib/main/lua.txt'))()
+
 
