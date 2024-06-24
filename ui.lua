aca827e587e7d9f2156a96f47c60e290f40e295e
Kh4ngHatesLjfe <149120276+Kh4ngHatesLjfe@users.noreply.github.com>
2024-06-24 12:58:04 -0700

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,7 +1,4 @@
-_G.KEY = '0f0bXkSA84nCSV7ruSCn8iARklACB84b5B38SN7A'
-_G.IsThemed = false
-_G.MainTitle = 'iWare'
-_G.MainTheme = ''
+
  
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Kh4ngHatesLjfe/lib/main/lua.txt'))()
 task.wait(1.2)
